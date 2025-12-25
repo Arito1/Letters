007a4da0ae10cfd26d31eb300cf2fd11d9e211e8
artur <artur.sapashev@narxoz.kz>
2025-12-25 22:52:39 +0500

    Fix dto

--- src/main/java/com/example/letters/config/WebConfig.java
+++ src/main/java/com/example/letters/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         "https://your-app-name.netlify.app"
                 )
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedMethods("GET","POST","PUT", "PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
