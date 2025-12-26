906e15cf6ba2d0b9789b22bb0618bb53dec64221
artur <artur.sapashev@narxoz.kz>
2025-12-26 08:34:29 +0500

    Config

--- src/main/java/com/example/letters/config/WebConfig.java
+++ src/main/java/com/example/letters/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://letters-front-end.onrender.com"
+                        "https://letters-front-end.onrender.com",
+                        "http://localhost:5173"
                 )
                 .allowedMethods("GET","POST","PUT", "PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
